ecdb8c1b69ab6432152c9d8f30f85dcbeb05d832
stever410 <ngotanduc410@gmail.com>
2021-12-14 00:22:29 +0700

    Update binding

--- lib/screens/login/login_binding.dart
+++ lib/screens/login/login_binding.dart
@@ -1,9 +1,11 @@
 import 'package:evds_staff/screens/login/login_controller.dart';
+import 'package:evds_staff/providers/auth_provider.dart';
 import 'package:get/get.dart';
 
 class LoginBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<AuthProvider>(() => AuthProvider());
     Get.lazyPut<LoginController>(() => LoginController());
   }
 }
--- lib/screens/main/main_binding.dart
+++ lib/screens/main/main_binding.dart
@@ -1,5 +1,6 @@
 import 'package:evds_staff/screens/evidence/evidence_controller.dart';
 import 'package:evds_staff/screens/main/main_controller.dart';
+import 'package:evds_staff/providers/auth_provider.dart';
 import 'package:evds_staff/screens/notification/notification_controller.dart';
 import 'package:evds_staff/screens/profile/profile_controller.dart';
 import 'package:get/get.dart';
@@ -7,6 +8,7 @@ import 'package:get/get.dart';
 class MainBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<AuthProvider>(() => AuthProvider());
     Get.lazyPut<MainController>(() => MainController());
     Get.lazyPut<NotificationController>(() => NotificationController());
     Get.lazyPut<EvidenceController>(() => EvidenceController());
