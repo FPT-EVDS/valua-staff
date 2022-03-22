a9b245a44d8a92df19246e4642f6b804527ea77d
stever410 <ngotanduc410@gmail.com>
2022-03-22 12:57:28 +0700

    Remove appUser in storage when 401

--- lib/providers/base_provider.dart
+++ lib/providers/base_provider.dart
@@ -35,6 +35,7 @@ class BaseProvider extends GetConnect {
       } catch (error) {
         _storage.remove(AppConstant.accessToken);
         _storage.remove(AppConstant.refreshToken);
+        _storage.remove(AppConstant.appUser);
       }
       return request;
     });
